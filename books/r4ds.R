bcc04646fa5a64c498f7d25913f97019c97a2ed0
hadley <h.wickham@gmail.com>
2016-12-01 10:18:35 -0600

    Scrape r4ds section headings

---
+++ books/r4ds.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(rvest)
+library(stringr)
+
+toc <- read_html("books/r4ds.html") %>% html_nodes(".chapter")
+
+id <- toc %>%
+  html_attr("data-level") %>%
+  forcats::fct_inorder()
+depth <- str_count(id, fixed(".")) + 1
+
+url <- toc %>%
+  html_node("a") %>%
+  html_attr("href") %>%
+  str_c("http://r4ds.had.co.nz/", .)
+
+title <- toc %>%
+  html_node(xpath =" .//b/following-sibling::text()") %>%
+  html_text(trim = TRUE)
+
+chapters <- tibble(id, title, depth, url)
+
+chapters %>%
+  filter(!is.na(title), depth <= 2) %>%
+  write_csv("books/r4ds.csv")
