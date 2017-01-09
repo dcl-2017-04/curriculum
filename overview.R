a4682ad1b47de7629af740e3cd80bd438330f6ac
hadley <h.wickham@gmail.com>
2017-01-09 16:04:44 -0600

    Better approach to node data

--- overview.R
+++ overview.R
@@ -3,18 +3,25 @@ source("utils.R")
 # ----------------
 library(ggraph)
 
+weeks <- syllabus %>%
+  map("units") %>%
+  discard(is.null) %>%
+  enframe(name = "week", value = "name") %>%
+  unnest(name) %>%
+  select(name, week) # igraph expects first column to be node name
+
 needs <- load_units() %>%
   map("needs") %>%
   discard(is.null) %>%
   enframe() %>%
   unnest(value)
 
-needs_graph <- igraph::graph_from_data_frame(needs)
+needs_graph <- igraph::graph_from_data_frame(needs, vertices = weeks)
 igraph::V(needs_graph)$unit <- gsub("-", "\n", names(igraph::V(needs_graph)))
 
 ggraph(needs_graph, layout = "sugiyama") +
   geom_edge_diagonal() +
-  geom_node_text(aes(label = unit)) +
+  geom_node_text(aes(label = name)) +
   scale_y_reverse() +
   theme_void()
 
