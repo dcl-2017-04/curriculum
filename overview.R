afc5ebdc323c66abe940f34439ceb4c3e9c8c7e0
hadley <h.wickham@gmail.com>
2017-01-23 08:15:13 -0600

    Make storyboard "visualisation"

--- overview.R
+++ overview.R
@@ -3,6 +3,40 @@ source("utils.R")
 # ----------------
 library(ggraph)
 
+
+# Storyboard --------------------------------------------------------------
+
+key_books <- books %>%
+  filter(is.na(depth) | depth < 3) %>%
+  transmute(book_id = forcats::fct_inorder(id))
+
+readings <- load_units() %>%
+  map("readings") %>%
+  map(. %>% keep(has_name, "book") %>% map_chr("book")) %>%
+  enframe(name = "unit", value = "book_id") %>%
+  unnest(book_id) %>%
+  mutate(book_id = factor(book_id, levels = levels(key_books$book_id)))
+
+unit_link <- function(x) {
+  sprintf("[%s](https://dcl-2017-01.github.io/curriculum/%s.html)", x, x)
+}
+
+load_syllabus() %>%
+  map("units") %>%
+  enframe("week", "unit") %>%
+  mutate(week = paste0("week", week)) %>%
+  unnest(unit) %>%
+  left_join(readings) %>%
+  group_by(week, book_id) %>%
+  summarise(units = paste0(unit_link(unit), collapse = ", ")) %>%
+  complete(key_books, fill = list(units = "")) %>%
+  spread(week, units) %>%
+  knitr::kable() %>%
+  cat(file = "storyboard.md", sep = "\n")
+
+
+# Graph -------------------------------------------------------------------
+
 weeks <- load_syllabus() %>%
   map("units") %>%
   discard(is.null) %>%
@@ -26,3 +60,4 @@ ggraph(needs_graph, layout = "sugiyama") +
   theme_void()
 
 ggsave("overview.png", width = 12, height = 6, dpi = 96)
+
